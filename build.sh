f3f739088bdabdc8bed7798bc120e8d9fdd9fede
D. Kasi Pavan Kumar <44864604+kasipavankumar@users.noreply.github.com>
2021-12-27 10:03:10 +0000

    :hammer: Add build scripts.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ENTRY_POINT=jscrates.js
+OUT_DIR=bin
+JS_OUT_FILE=jscrates.cjs
+
+# Build JS using `esbuild`
+npx esbuild $ENTRY_POINT --bundle --minify --platform=node --outfile=$OUT_DIR/$JS_OUT_FILE
+
+# Compile JS into executable
+npx pkg $OUT_DIR/$JS_OUT_FILE --out-path $OUT_DIR
