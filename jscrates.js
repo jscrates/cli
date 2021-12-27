0b7a52763f0ce7f72c1496a607eeaedfaf240a2e
D. Kasi Pavan Kumar <44864604+kasipavankumar@users.noreply.github.com>
2021-12-27 10:02:22 +0000

    :sparkles: Add configuration file utilities.

--- jscrates.js
+++ jscrates.js
@@ -5,14 +5,21 @@
 import { Command } from 'commander'
 
 import downloadPackage from './actions/download.js'
+import login from './actions/auth/login.js'
 import publishPackage from './actions/publish.js'
+import { createConfigFileIfNotExists } from './utils/config-file.js'
 
+createConfigFileIfNotExists({
+  createdAt: Date.now(),
+})
 const program = new Command()
 
 program
   .name('jscrates')
   .description(`Welcome to JSCrates 📦, yet another package manager for Node`)
   .version(`v2.2.0`, '-v, --version', 'display current version')
+
+program.command('login').description('Login to JSCrates').action(login)
 
 program
   .command('download')
