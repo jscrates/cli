3f7cc9fdd007d57792a51c9a7255662be66adeb3
D. Kasi Pavan Kumar <44864604+kasipavankumar@users.noreply.github.com>
2021-12-28 10:50:15 +0000

    :sparkles: Add auth actions.

---
+++ actions/auth/register.js
@@ -0,0 +1,44 @@
+import Spinner from 'mico-spinner'
+import { registerUser } from '../../lib/api/actions.js'
+import { logError, logSuccess } from '../../utils/loggers.js'
+import {
+  confirmationPrompt,
+  promptCredentials,
+} from '../../utils/prompts/index.js'
+
+function register(config) {
+  const spinner = Spinner('Registering a new user')
+
+  return async function () {
+    try {
+      if (config?.has('auth.token')) {
+        if (
+          !(await confirmationPrompt(
+            `It seems like you are already logged in`,
+            `\nCreating a new account will log you out of the current account.`,
+            `\nDo you want to continue? (y/n)`
+          ))
+        ) {
+          return process.exit(1)
+        }
+
+        config.delete('auth.token')
+        console.clear()
+      }
+
+      const { email, password } = await promptCredentials()
+
+      spinner.start()
+
+      const status = await registerUser({ email, password })
+
+      spinner.succeed()
+      return logSuccess(status)
+    } catch (error) {
+      spinner.fail()
+      return logError(error)
+    }
+  }
+}
+
+export default register
